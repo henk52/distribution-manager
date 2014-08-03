dc70025dca0835591a01af1c14277802b3d38ddb
henk52 <henk52@spam.no>
2014-08-03 19:05:10 +0200

    With firewall hack in there.

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,20 +8,34 @@
 #   - nfs
 #   - the git tools.
 
+$szGitTopDir = '/var/git'
+# TODO C define the GIT user.
+# TODO C Create git dir.
+
 $arAliases = {
-  'git' => '/var/git',
-  'test' => '/var/test',
+  'git' => "$szGitTopDir",
 }  
 
+# TODO C depend lighttpd on the GIT class
 class { "lighttpd":
   harAliasMappings => $arAliases,
 }
 
 # TODO C open the Firewalld ports.
-# define a service
-firewalld::service { 'web':
-        description     => 'Web service',
-        ports           => [{port => '80', protocol => 'tcp',},],
+firewalld::zone { 'public':
+  services => ['ssh', 'http', 'vnc-server'],
+}
+
+
+# TODO C Remove before launch. This is suppose to be part of the firewalld class.
+# This is a workaround to the error of missing Package support.
+#  See: https://github.com/jpopelka/puppet-firewalld/issues/1
+package { 'firewalld':
+  ensure => present,
+}
+service { 'firewalld':
+                ensure     => running,  # ensure it's running
+                enable     => true,     # start on boot
 }
 
 # TODO C Disable selinux.
